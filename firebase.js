2d32d10de60a28b7fb258f83964aaa0f1a2e28e7
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2021-05-10 20:34:16 +0530

    Login and Loading screen done

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxLHoSOU7BEEC7bS8cYSLNhGnoFIUMxeg",
+  authDomain: "whatsapp-28bd1.firebaseapp.com",
+  projectId: "whatsapp-28bd1",
+  storageBucket: "whatsapp-28bd1.appspot.com",
+  messagingSenderId: "254007986097",
+  appId: "1:254007986097:web:e54076219d0196bf855f90"
+};
+
+const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+
+const db = app.firestore;
+const auth = app.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {db, auth, provider};
